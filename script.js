ed16e707a037a974dc465539db0c6a5d1589a629
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-11-20 11:18:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function btn() {
+  let updatePtag = document.getElementById("status");
+  let head = document.createElement("h1");
+  updatePtag.innerText = "Entered Metaverse";
+  head.innerHTML = updatePtag.innerHTML;
+  updatePtag.parentNode.replaceChild(head, updatePtag);
+  console.log(updatePtag);
+}
